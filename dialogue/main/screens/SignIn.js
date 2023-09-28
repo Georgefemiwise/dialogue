03549a720216372dfe300b1aaf415b993364a700
richod1 <frimpsup@gmail.com>
2023-09-27 21:30:27 -0400

    D-01 auth

---
+++ dialogue/main/screens/SignIn.js
@@ -0,0 +1,17 @@
+import {View,Text,StyleSheet} from "react-native"
+import React from "react"
+
+
+const SignIn=()=>{
+    return(
+        <View style={styles.container}>
+            <Text>SignIn page</Text>
+        </View>
+    )
+}
+export default SignIn;
+const styles=StyleSheet.create({
+    container:{
+
+    }
+})
---
+++ dialogue/main/screens/SignUp.js
@@ -0,0 +1,18 @@
+import {Text,View} from "react-native"
+import React from "react"
+
+const SignUp=()=>{
+    return(
+        <View style={styles.container}>
+            <Text>SignUp page</Text>
+        </View>
+    )
+}
+
+export default SignUp;
+
+const styles=StyleSheet.create({
+    container:{
+        
+    }
+})
