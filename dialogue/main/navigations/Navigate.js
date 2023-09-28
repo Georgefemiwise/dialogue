8f2c2393a22b8732270795106e8163eb2843ca99
richod1 <frimpsup@gmail.com>
2023-09-27 22:41:18 -0400

    D-01 auth

---
+++ dialogue/main/navigations/Navigate.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {View} from "react-native"
+
+const Navigate=()=>{
+    return(
+        <View>
+            {/* natigation init */}
+        </View>
+    )
+}
---
+++ dialogue/main/screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {View,Text,StyleSheet} from "react-native"
+
+
+const Home=()=>{
+    return(
+        <View style={styles.container}>
+<Text>HOME PAGE</Text>
+        </View>
+    )
+}
+export default Home;
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+
+    }
+})
